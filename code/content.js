b06817a1c1fc8d5a27b81f377441fbf0a2de6cbd
Michael Brave <mbrave@e1z1r7p26.42.us.org>
2019-05-18 14:07:38 -0700

    dialog

--- code/content.js
+++ code/content.js
@@ -3,155 +3,29 @@ module.exports = [
   // tags are optional and used for "find facts about [tag]" etc
   // image is optional - displayed on UI
   // START PASTE FROM SPREADSHEET HERE
-  {
-    text: "The world’s oldest piece of chewing gum is over 9000 years old!",
-    image: {
-      url: "images/gum.png"
-    }
-  },
-  {
-    text: "A sneeze travels about 100 miles per hour",
-    image: {
-      url: "images/sneeze.png"
-    }
-  },
-  {
-    text: "It would take a sloth one month to travel one mile",
-    image: {
-      url: "images/sloth.png"
-    }
-  },
-  {
-    text: "10% of the World’s population is left handed"
-  },
-  {
-    text: "Computers went by the name 'Electronic Brains' in the 1950s",
-    image: {
-      url: "images/computer_brain.png"
-    }
-  },
-  {
-    text: "A broken clock is right two times every day",
-    image: {
-      url: "images/clock.png"
-    }
-  },
-  {
-    text: "A mole can dig a tunnel that is 300 feet long in only one night"
-  },
-  {
-    text: "A crocodile can’t poke its tongue out",
-    image: {
-      url: "images/crocodile.png"
-    }
-  },
-  {
-    text: "Blueberries will not ripen until they are picked"
-  },
-  {
-    text: "Honey never spoils",
-    image: {
-      url: "images/honey.png"
-    }
-  },
-  {
-    text: "The first-ever webcam was used to watch a pot of coffee",
-    image: {
-      url: "images/coffee_pot.png"
-    }
-  },
-  {
-    text: "In New York it is illegal to sell a haunted house without telling the buyer",
-    image: {
-      url: "images/haunted_house.png"
-    }
-  },
-  {
-    text: "Human thigh bones are stronger than concrete"
-  },
-  {
-    text: "The male ostrich can roar just like a lion",
-    image: {
-      url: "images/ostrich.png"
-    }
-  },
-  {
-    text: "Scientists have tracked butterflies that travel over 3000 miles",
-    image: {
-      url: "images/butterfly.png"
-    }
-  },
-  {
-    text: "If 33 million people held hands they could make it all the way around the equator",
-    image: {
-      url: "images/earth.png"
-    }
-  },
-  {
-    text: "The term 'tera' in terrabyte is from the greek word for monster",
-    image: {
-      url: "images/monster.png"
-    }
-  },
-  {
-    text: "The tongue of a blue whale weighs more than most elephants",
-    image: {
-      url: "images/whale.png"
-    }
-  },
-  {
-    text: "A cow gives nearly 200,000 glasses of milk in a lifetime",
-    image: {
-      url: "images/cow.png"
-    }
-  },
-  {
-    text: "Dolphins sleep with one eye open",
-    image: {
-      url: "images/dolphin.png"
-    }
-  },
-  {
-    tags: ["dog", "dogs"],
-    text: "What do you call a cold dog?... A chili dog!",
-    image: {
-      url: "images/dog1.jpg"
-    }
-  },
-  {
-    tags: ["dog", "dogs"],
-    text: "What do a dog and cell phone have in common?... Both have collar ID!",
-    image: {
-      url: "images/dog2.png"
-    }
-  },
-  {
-    tags: ["dog", "dogs"],
-    text: "What kind of dog doesn't bark?... A hush puppy!",
-    image: {
-      url: "images/dog3.jpg"
-    }
-  },
-  {
-    tags: ["cat", "cats"],
-    text: "What do cats like to eat on a hot day?... Mice Cream",
-    image: {
-      url: "images/cat1.png"
-    }
-  },
-  {
-    tags: ["cat", "cats"],
-    text: "Why was the cat afraid of the tree? Because of it's bark!",
-    image: {
-      url: "images/cat2.png"
-    }
-  },
-  {
-    tags: ["cat", "cats"],
-    text: "What type of cats go bowling?... Alley cats!",
-    image: {
-      url: "images/cat3.jpg"
-    }
-  },
+  //
+  // leave this as an example
+  // {
+  //   text: "The world’s oldest piece of chewing gum is over 9000 years old!",
+  //   image: {
+  //     url: "images/gum.png"
+  //   }
+  // },
+  {tags: ["magic","magician"], text: "Are you a magician? Because whenever I look at you, everyone else disappears!", image: {url: "http://clipart-library.com/img/2034420.png"}},
+{tags: ["photo","picture","photographer"], text: "I’m not a photographer, but I can picture me and you together.", image: {url: "http://clipart-library.com/image_gallery2/Photo-Camera-Free-Download-PNG.png"}},
+{tags: ["disney","happy"], text: "They say Disneyland is the happiest place on earth. Well apparently, no one has ever been standing next to you.", image: {url: "http://clipart-library.com/images/8TAbM8o7c.jpg"}},
+{tags: ["feeling","on"], text: "For some reason, I was feeling a little off today. But when you came along, you definitely turned me on."},
+{tags: ["phone","number","lost"], text: "I seem to have lost my phone number. Can I have yours?"},
+{text: "Are you a parking ticket? ‘Cause you’ve got fine written all over you."},
+{tags: ["tired","running"], text: "Are you sure you’re not tired? You’ve been running through my mind all day."},
+{tags: ["boxer","boxing"], text: "Was your dad a boxer? Because damn, you’re a knockout!"},
+{tags: ["change","name"], text: "There’s only one thing I want to change about you, and that’s your last name."},
+{tags: ["sexy","job","career"], text: "Aside from being sexy, what do you do for a living?"},
+{tags: ["dinosaur"], text: "Kiss me if I’m wrong, but dinosaurs still exist, right?"},
+{tags: ["forever","nothing"], text: "If nothing lasts forever, will you be my nothing?"},
+{tags: ["eyes"], text: "There must be something wrong with my eyes, I can’t take them off you."},
+{tags: ["talking"], text: "I’m sorry, were you talking to me? [No] Well then, please start."},
+{tags: ["math","maths","mathemetician","number"], text: "I’m no mathematician, but I’m pretty good with numbers. Tell you what, give me yours and watch what I can do with it."},
+{tags: ["bet","drinks"], text: "My buddies bet me that I wouldn’t be able to start a conversation with the hottest person in the bar. Wanna buy some drinks with their money?"},
   // END PASTE FROM SPREADSHEET HERE
 ]
