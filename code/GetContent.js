e4c807a43507d04a6c7c85396c046e6eb4d6d8c3
Vincent Dauvergne <vdauverg@e1z1r7p28.42.us.org>
2019-05-18 13:41:18 -0700

    setup random fact generator

---
+++ code/GetContent.js
@@ -0,0 +1,30 @@
+const UTIL = require("./lib/util");
+const CONTENT = require("./content");
+
+// GetContent
+exports.function = function (searchTerm) {
+  //You can replace with a call to a web api - make sure you map api response to content model
+  var content = CONTENT
+
+  //filter based on searchTerm (note that if you use a web api then filtering can be done in the web API itself)
+  if (searchTerm) {
+    content = UTIL.findContent(content, searchTerm)
+  }
+  
+  //pick a random content
+  var chosenContent;
+  if (content.length) {
+    var index = Math.floor(content.length * Math.random())
+    chosenContent = content[index]
+  }
+
+  // return content if exists, else null (No Result)
+  if (chosenContent) {
+    return {
+      text: chosenContent.text,
+      image: chosenContent.image
+    }
+  } else {
+    return null;
+  }
+}
